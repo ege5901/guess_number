46e7fd0fcb08da540c73549106cde6cd87d6638b
Egemen Özdilek <egeoozdilek@gmail.com>
2019-07-23 23:07:43 +0300

    beta ver 0.9

--- main.js
+++ main.js
@@ -1,11 +1,51 @@
-button = document.getElementById('button')
+label = document.getElementById('label');
+scorelabel = document.getElementById('score');
+button = document.getElementById('button');
+input = document.getElementById("input");
+
+/*
+// TODO:
+  score ekle
+  2. aşamada input gizlensin
+  bugları çöz
+
+  */
 
 
 function buttonmouseover() {
-  button.innerHTML = "ilerlemek için tıkla"
+  button.innerHTML = "ilerlemek için tıkla";
 
 }
 
 function buttononmouseleave() {
-  button.innerHTML = "sonraki"
+  button.innerHTML = "sonraki";
+}
+function random() {
+  return Math.floor((Math.random()*10)+1);
+}
+
+var stage = 2;
+var score = 0;
+
+function next() {
+
+  if (window.stage == 1) {
+    window.scorelabel.innerHTML = ("score: " + window.score);
+    window.label.innerHTML = ("1 İle 10 Arasından Bi Sayı Tuttum Tahmin Et");
+    window.stage = 2;
+  }
+
+  else if (window.stage == 2) {
+    var guess = window.input.value;
+    var random = window.random();
+    if (guess == random) {
+      window.label.innerHTML = "AFFERİN LEN";
+    }
+    else {
+      window.label.innerHTML = ":( cevap şuydu:"+ random;
+
+    }
+
+    window.stage = 1;
+  }
 }
